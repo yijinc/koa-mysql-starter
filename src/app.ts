aff9f6b12fffc251f85a383aa70a83ae49c8e159
yijinc <yijinc@qq.com>
2021-10-20 11:23:34 +0800

    feat: implement first middleware

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import Koa from 'koa';
 import bodyParser from 'koa-bodyparser';
 
+import logger from './middleware/logger';
+
 // 初始化 Koa 应用实例
 const app = new Koa();
 
 // 注册中间件
 app.use(bodyParser());
+app.use(logger());
 
 // 响应用户请求
 app.use((ctx) => {
