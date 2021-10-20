2060916d5bd8d9353d5f3f8625e8687e988c7e4e
yijinc <yijinc@qq.com>
2021-10-20 11:11:42 +0800

    feat: init app

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import Koa from 'koa';
+import bodyParser from 'koa-bodyparser';
+
+// 初始化 Koa 应用实例
+const app = new Koa();
+
+// 注册中间件
+app.use(bodyParser());
+
+// 响应用户请求
+app.use((ctx) => {
+  ctx.body = 'Hello Koa';
+});
+
+// 运行服务器
+app.listen(3000);
