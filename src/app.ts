570b07d11fa990e14b39cc73740ab34fc0ba5ba3
yijinc <yijinc@qq.com>
2021-10-21 14:58:43 +0800

    feat: add typeorm mysql

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,37 @@
 import Koa from 'koa';
 import bodyParser from 'koa-bodyparser';
 
+import 'reflect-metadata'; // shim for typeorm
+import { createConnection } from 'typeorm';
+
 import users from './routers/users';
 import posts from './routers/posts';
 import logger from './middleware/logger';
 
-// 初始化 Koa 应用实例
-const app = new Koa();
 
-// 注册中间件
-app.use(bodyParser());
-app.use(logger());
+createConnection()
+  .then(() => {
+    startApp();
+  })
+  .catch((err: string) => console.log('TypeORM connection error:', err));
 
-// app.use((ctx) => {
-//   ctx.body = 'Hello Koa';
-// });
+const startApp = () => {
+  // 初始化 Koa 应用实例
+  const app = new Koa();
 
-// 响应用户请求
-app.use(users.routes()).use(users.allowedMethods());
-app.use(posts.routes()).use(posts.allowedMethods())
+  // 注册中间件
+  app.use(bodyParser());
+  app.use(logger());
 
-// 运行服务器
-app.listen(3000);
+  // app.use((ctx) => {
+  //   ctx.body = 'Hello Koa';
+  // });
+
+  // 响应用户请求
+  app.use(users.routes()).use(users.allowedMethods());
+  app.use(posts.routes()).use(posts.allowedMethods())
+
+  // 运行服务器
+  app.listen(3000);
+
+};
